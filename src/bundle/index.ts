af3069e0a45aa24540d1354453581c86d6a3485f
IvanOrsh <ksironov@mail.ru>
2023-01-21 03:06:20 +0300

    minor project restructuring src/plugins -> src/bundle/plugins

--- src/bundle/index.ts
+++ src/bundle/index.ts
@@ -1,7 +1,7 @@
 import * as esbuild from "esbuild-wasm";
 
-import { fetchPlugin } from "../plugins/fetch-plugin";
-import { unpkgPathPlugin } from "../plugins/unpkg-path-plugins";
+import { fetchPlugin } from "./plugins/fetch-plugin";
+import { unpkgPathPlugin } from "./plugins/unpkg-path-plugins";
 
 export const bundle = async (rawCode: string) => {
   const result = await esbuild.build({
