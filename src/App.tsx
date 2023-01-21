6f076185eb9c395859ab51f62c20bd6f279f4857
IvanOrsh <ksironov@mail.ru>
2023-01-21 03:00:49 +0300

    bundling logic extracted from App.tsx to src/bundle/index.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
 import React, { useState, useRef } from "react";
-import * as esbuild from "esbuild-wasm";
 
 import CodeEditor from "./components/code-editor";
 import Preview from "./components/preview";
-import { fetchPlugin } from "./plugins/fetch-plugin";
-import { unpkgPathPlugin } from "./plugins/unpkg-path-plugins";
+import bundle from "./bundle";
 
 const App: React.FC = () => {
   const [input, setInput] = useState("");
   const [code, setCode] = useState("");
 
   const onClick = async () => {
-    const result = await esbuild.build({
-      entryPoints: ["index.js"],
-      bundle: true,
-      write: false,
-      plugins: [unpkgPathPlugin(), fetchPlugin(input)],
-      define: {
-        // it's not going to fix itself
-        // "process.env.NODE_ENV": '"production"',
-        global: "window",
-      },
-    });
-
-    setCode(result.outputFiles[0].text);
+    const output = await bundle(input);
+    setCode(output);
   };
 
   return (
---
+++ src/bundle/index.ts
@@ -0,0 +1,20 @@
+import * as esbuild from "esbuild-wasm";
+
+import { fetchPlugin } from "../plugins/fetch-plugin";
+import { unpkgPathPlugin } from "../plugins/unpkg-path-plugins";
+
+export default async (rawCode: string) => {
+  const result = await esbuild.build({
+    entryPoints: ["index.js"],
+    bundle: true,
+    write: false,
+    plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+    define: {
+      // it's not going to fix itself
+      // "process.env.NODE_ENV": '"production"',
+      global: "window",
+    },
+  });
+
+  return result.outputFiles[0].text;
+};
