7ed5adb72c49b66ad551b283023402921e28cc74
IvanOrsh <ksironov@mail.ru>
2023-01-24 02:19:41 +0300

    WIP bundlesReducer scaffolding

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -1,1 +1,29 @@
-export default 1;
+import produce from "immer";
+
+import { ActionType } from "../action-types";
+import { Action } from "../actions";
+
+interface BundlesState {
+  [key: string]: {
+    loading: boolean; // bundling
+    code: string;
+    error: string;
+  };
+}
+
+const initialState: BundlesState = {};
+
+const reducer = produce(
+  (state: BundlesState = initialState, action: Action) => {
+    if (action.type === ActionType.BUNDLE_START) {
+      //..
+    }
+
+    if (action.type === ActionType.BUNDLE_COMPLETE) {
+      // ..
+    }
+  },
+  initialState
+);
+
+export default reducer;
