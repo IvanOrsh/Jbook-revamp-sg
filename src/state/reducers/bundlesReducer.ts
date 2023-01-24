c9ec2eb856e590b2853a3db9ca3599d5bc3b50e7
IvanOrsh <ksironov@mail.ru>
2023-01-24 03:53:29 +0300

    createBundle action-creator added

--- src/state/reducers/bundlesReducer.ts
+++ src/state/reducers/bundlesReducer.ts
@@ -7,7 +7,7 @@ interface BundlesState {
   [key: string]: {
     loading: boolean; // bundling
     code: string;
-    error: string;
+    err: string;
   };
 }
 
@@ -16,11 +16,19 @@ const initialState: BundlesState = {};
 const reducer = produce(
   (state: BundlesState = initialState, action: Action) => {
     if (action.type === ActionType.BUNDLE_START) {
-      //..
+      state[action.payload.cellId] = {
+        loading: true,
+        code: "",
+        err: "",
+      };
     }
 
     if (action.type === ActionType.BUNDLE_COMPLETE) {
-      // ..
+      state[action.payload.cellId] = {
+        loading: false,
+        code: action.payload.bundle.code,
+        err: action.payload.bundle.err,
+      };
     }
   },
   initialState
